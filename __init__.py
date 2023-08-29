dd584fd8fa7fc60418679cb2a498b499cb12175b
Chris <chris.goringe@gmail.com>
2023-08-29 13:12:49 +1000

    added more nodes and docs

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ from randoms import *
 NODE_CLASS_MAPPINGS = { "Random Int" : RandomInt,
                         "Random Float" : RandomFloat,
                         "Load Random Lora" : LoadRandomLora,
+                        "Load Random Image" : LoadRandomImage,
                       }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
--- randoms.py
+++ randoms.py
@@ -1,6 +1,9 @@
 import random
 from common import *
-from nodes import LoraLoader
+from nodes import LoraLoader, CheckpointLoaderSimple
+from PIL import Image, ImageOps
+import numpy as np
+import torch
   
 class SeedContext():
     """
@@ -17,24 +20,16 @@ class SeedContext():
         random.setstate(self.state)
 
 class RandomBase(Base_randoms):
-    RETURN_NAMES = ("rand",)
     CATEGORY = "randoms"
-    def IS_CHANGED(self, minimum, maximum, seed):
+    def IS_CHANGED(self, **kwargs):
         return random.random()
-    def func(self, minimum, maximum, seed, **kwargs):
-        with SeedContext(seed):
-            rand = self.gen(minimum, maximum)
-            if 'decimal_places' in kwargs:
-                rand = round(rand, kwargs['decimal_places'])
-        return (rand,)
 
 def SEED_INPUT():
     with SeedContext(None):
         return ("INT",{"default": random.randint(1,999999999), "min": 0, "max": 0xffffffffffffffff})
 
-class RandomFloat(Base_randoms):
+class RandomFloat(RandomBase):
     RETURN_NAMES = ("random_float",)
-    CATEGORY = "randoms"
     REQUIRED = { 
                 "minimum": ("FLOAT", {"default": 0.0}), 
                 "maximum": ("FLOAT", {"default": 1.0}), 
@@ -47,9 +42,8 @@ class RandomFloat(Base_randoms):
             rand = round(random.uniform(minimum, maximum), decimal_places)
         return (rand,)
 
-class RandomInt(Base_randoms):
+class RandomInt(RandomBase):
     RETURN_NAMES = ("random_int",)
-    CATEGORY = "randoms"
     REQUIRED = { 
                 "minimum": ("INT", {"default": 0}), 
                 "maximum": ("INT", {"default": 99999999}), 
@@ -61,29 +55,77 @@ class RandomInt(Base_randoms):
             rand = random.randint(minimum, maximum)
         return (rand,)
 
-class LoadRandomLora(Base_randoms, LoraLoader):
-    CATEGORY = "randoms"
-    systematic_index = -1
+def from_list(seed, list, index):
+    if seed!=0:
+        with SeedContext(seed):
+            return (random.choice(list), index)
+    else:
+        index = (index+1)%len(list)
+        return (list[index], index)
+
+class LoadRandomLora(RandomBase, LoraLoader):
     @classmethod
     def INPUT_TYPES(s):
         i = LoraLoader.INPUT_TYPES()
         i['required'].pop('lora_name')
         i['required']['seed'] = SEED_INPUT()
+        i['optional'] = s.OPTIONAL
         return i
-    
     RETURN_TYPES = ("MODEL", "CLIP", "STRING",)
     RETURN_NAMES = ("model", "clip", "lora_name",)
+    OPTIONAL = {"lora_name": ("STRING", {"default":""})}
 
     def __init__(self):
+        self.systematic_index = -1
         LoraLoader.__init__(self)
 
-    def func(self, model, clip, strength_model, strength_clip, seed):
+    def func(self, model, clip, strength_model, strength_clip, seed, lora_name=""):
         loras = get_config_randoms('lora_names', exception_if_missing_or_empty=True)
-        if seed!=0:
-            with SeedContext(seed):
-                lora_name = random.choice(loras)
-                lora_name = lora_name if '.' in lora_name else lora_name + ".safetensors" 
-        else:
-            lora_name = loras[self.systematic_index]
-            self.systematic_index = (self.systematic_index + 1) % len(loras)
+        if lora_name=="":
+            lora_name, self.systematic_index = from_list(seed, loras, self.systematic_index)
+        lora_name = lora_name if '.' in lora_name else lora_name + ".safetensors" 
         return self.load_lora(model, clip, lora_name, strength_model, strength_clip) + (lora_name,)
+
+class LoadRandomCheckpoint(RandomBase, CheckpointLoaderSimple):
+    systematic_index = -1
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "seed": SEED_INPUT() }, "optional": {"ckpt_name": ("STRING", {"default":""})} }
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE", "STRING",)
+    RETURN_NAMES = ("model", "CLIP", "VAE", "ckpt_name",)
+
+    def func(self, seed, ckpt_name=""):
+        checkpoints = get_config_randoms('checkpoint_names', exception_if_missing_or_empty=True)
+        if ckpt_name=="":
+            ckpt_name, self.systematic_index = from_list(seed, checkpoints, self.systematic_index)
+        ckpt_name = ckpt_name if '.' in ckpt_name else ckpt_name + '.safetensors'
+        return self.load_checkpoint(ckpt_name) + (ckpt_name,)
+
+class LoadRandomImage(RandomBase):
+    def __init__(self):
+        self.systematic_index = -1
+    REQUIRED = { "folder": ("STRING", {} ), "seed": SEED_INPUT() }
+    RETURN_TYPES = ("IMAGE","STRING",)
+    RETURN_NAMES = ("image","filename",)
+    OPTIONAL = {"filename": ("STRING", {"default":""})}
+
+    def get_filenames(self, folder):
+        image_extensions = get_config_randoms('image_extensions', exception_if_missing_or_empty=True)
+        def is_image_filename(filename):
+            split = os.path.splitext(filename)
+            return len(split)>0 and split[1] in image_extensions
+        files = [file for file in os.listdir(folder) if is_image_filename(file)]
+        if len(files)==0:
+            raise Exception(f"No files matching {image_extensions} in {folder}")
+        return files
+
+    def func(self, folder, seed, filename=""):
+        if filename=="":
+            filename, self.systematic_index = from_list(seed, self.get_filenames(folder), self.systematic_index)
+        filepath = os.path.join(folder, filename)
+        i = Image.open(filepath)
+        i = ImageOps.exif_transpose(i)
+        image = i.convert("RGB")
+        image = np.array(image).astype(np.float32) / 255.0
+        image = torch.from_numpy(image)[None,]
+        return (image, filename, )
