4791054494d3c648505751f46601e128780698c2
Chris <chris.goringe@gmail.com>
2023-09-12 09:03:14 +1000

    SeedContext

--- randoms.py
+++ randoms.py
@@ -1,25 +1,12 @@
 import random
 from .common import get_config_randoms
-from custom_nodes.cg_custom_core.base import BaseNode
+from custom_nodes.cg_custom_core.base import BaseNode, SeedContext
 from nodes import LoraLoader, CheckpointLoaderSimple
 from PIL import Image, ImageOps
 import numpy as np
 import torch
 import os
   
-class SeedContext():
-    """
-    Context Manager to allow one or more random numbers to be generated, optionally using a specified seed, 
-    without changing the random number sequence for other code.
-    """
-    def __init__(self, seed=None):
-        self.seed = seed
-    def __enter__(self):
-        self.state = random.getstate()
-        if self.seed:
-            random.seed(self.seed)
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        random.setstate(self.state)
 
 class RandomBase(BaseNode):
     CATEGORY = "randoms"
