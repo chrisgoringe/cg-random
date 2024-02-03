fe5b045bc34e667209dc5ef0fafce3d181829f4f
Chris <chris.goringe@gmail.com>
2024-02-03 13:16:37 +1100

    remove custom core

--- randoms.py
+++ randoms.py
@@ -1,11 +1,50 @@
 import random
 from .common import get_config_randoms
-from custom_nodes.cg_custom_core.base import BaseNode, SeedContext
 from nodes import LoraLoader, CheckpointLoaderSimple
 from PIL import Image, ImageOps
 import numpy as np
 import torch
 import os
+
+
+class BaseNode:
+    def __init__(self):
+        pass
+    FUNCTION = "func"
+    REQUIRED = {}
+    OPTIONAL = None
+    HIDDEN = None
+    @classmethod    
+    def INPUT_TYPES(s):
+        types = {"required": s.REQUIRED}
+        if s.OPTIONAL:
+            types["optional"] = s.OPTIONAL
+        if s.HIDDEN:
+            types["hidden"] = s.HIDDEN
+        return types
+    RETURN_TYPES = ()
+    RETURN_NAMES = ()
+
+class classproperty(object):
+    def __init__(self, f):
+        self.f = f
+    def __get__(self, obj, owner):
+        return self.f(owner)
+    
+class SeedContext():
+    """
+    Context Manager to allow one or more random numbers to be generated, optionally using a specified seed, 
+    without changing the random number sequence for other code.
+    """
+    def __init__(self, seed=None):
+        self.seed = seed
+    def __enter__(self):
+        self.state = random.getstate()
+        if self.seed:
+            random.seed(self.seed)
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        random.setstate(self.state)
+
   
 class SystematicBase(BaseNode):
     CATEGORY = "randoms"
